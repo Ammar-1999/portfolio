62356fca8c8dfc3530ecb6b435bcaa1fa8a66414
Ammar1999y <thesyclashers.911@gmail.com>
2024-06-23 23:51:05 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,74 @@
+'use client'
+import { useRef } from "react";
+import Navbar from "@/components/navbar";
+import { Spotlight } from "@/components/ui/spotlight";
+
+import WebsiteDesign from "./website-design";
+import GraphicDesign from "./graphic-design";
+import Services from "./services";
+import FAQS from "./faq";
+import { InfiniteMovingCardsDemo } from "./snippets/infinite-moving-card-snippet";
+
+export default function Home() {
+  const websiteDesignRef = useRef<HTMLDivElement>(null);
+  const graphicDesignRef = useRef<HTMLDivElement>(null);
+  const shopifyStoresRef = useRef<HTMLDivElement>(null);
+  const brandsRef = useRef<HTMLDivElement>(null);
+  const servicesRef = useRef<HTMLDivElement>(null);
+
+  const scrollToWebsiteDesign = () => {
+    websiteDesignRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  };
+
+  const scrollToGraphicDesign = () => {
+    graphicDesignRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToShopifyStores = () => {
+    shopifyStoresRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToBrands = () => {
+    brandsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // Function to scroll to Services section
+  const scrollToServices = () => {
+    servicesRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Navbar
+        scrollToWebsiteDesign={scrollToWebsiteDesign}
+        scrollToGraphicDesign={scrollToGraphicDesign}
+        scrollToShopifyStores={scrollToShopifyStores}
+        scrollToBrands={scrollToBrands}
+        scrollToServices={scrollToServices}
+      />
+
+      <Spotlight className="hidden md:flex md:-top-80 left-80  " fill="white" />
+      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
+        <div className="text-4xl pb-5 md:text-7xl px-6 text-center  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+          Create, grow, and <br /> scale your business
+        </div>
+
+        <div ref={websiteDesignRef}>
+          <WebsiteDesign />
+        </div>
+        <div ref={graphicDesignRef}>
+          <GraphicDesign />
+        </div>
+        <div id ='services'>
+        <Services />
+        </div>
+        <InfiniteMovingCardsDemo />
+        <FAQS />
+      </div>
+    </div>
+  );
+}
